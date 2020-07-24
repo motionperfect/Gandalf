924c8ace685cd4d6478a49d0fad8304fb86c58ae
Julien Sarriot <julien.sarriot@epitech.eu>
2020-07-24 10:29:56 +0200

    Reformatted files

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
+
+import { AppService } from "./app.service";
+import { AppController } from "./app.controller";
+import { AuthGuard } from "./guards";
+
+const Services = [
+  AppService
+];
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController
+  ],
+  providers: [
+    ...Services,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
+  ]
 })
 export class AppModule {}
