86b74af17b27211debd8b635c6de11f2436163b5
Julien Sarriot <julien.sarriot@epitech.eu>
2020-09-09 23:33:38 +0200

    Added constraints on BCRYPT_ROUNDS env

--- src/config/app/app.schema.ts
+++ src/config/app/app.schema.ts
@@ -1,8 +1,8 @@
-import * as Joi from "@hapi/joi";
+import * as Joi from '@hapi/joi'
 
 export default {
 
   APP_PORT: Joi.number().integer().min(80).max(65535).default(3000),
 
-  BCRYPT_ROUNDS: Joi.number().integer().positive().default(10)
-};
+  BCRYPT_ROUNDS: Joi.number().integer().positive().min(8).max(16).default(10),
+}
